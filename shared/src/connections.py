1d05f642420c5530eabfb1564ca9a19e69aacf45
Ben Stuart <choncy@rocketmail.com>
2018-01-22 14:54:37 +0000

    pep8 ConnectionHandler

--- shared/src/connections.py
+++ shared/src/connections.py
@@ -1,36 +1,57 @@
-from socket import *
+from socket import socket, gethostname, gethostbyname, AF_INET, SOCK_STREAM
 import threading
 
 class Connection:
-    def __init__(self, host, port, connection = None):
+    """
+    Class the represent a TCP connection
+    """
+    def __init__(self, host, port, connection=None):
+        """
+        Create base Connection object
+
+        :param host: location of other party
+        :param port: port number of other party
+        :param connection: default new connection, can be passed existing tcp socket
+        """
         self._host = gethostbyname(host) #resolve hostnames
         self._port = port
-        if connection == None:
+        if connection is None:
             self._socket = socket(AF_INET, SOCK_STREAM)
-            self._OPEN_STATUS = False
+            self._open_status = False
         else:
             self._socket = connection
-            self._OPEN_STATUS = True
+            self._open_status = True
 
-    def send(self, message, wait_response = False):
-        if self._OPEN_STATUS:
+    def send(self, message, wait_response=False):
+        """
+        Send message to other party over TCP
+
+        :param message: Message (String) to be sent
+        :param wait_response: (bool) default False, set True to return message response
+        :return: response to message else None
+        """
+        if self._open_status:
             try:
-                msglen = "{:16}".format(len(message)) #create padded string of length 16 containing the length of the message to be sent
+                msglen = "{:16}".format(len(message)) #create padded string
                 message = msglen + message #add the message length to the begining of the message
                 self._socket.sendall(message.encode()) #send encoded message over TCP
                 if wait_response:
-                    return recv() #recv message in response
-                else:
-                    return None
+                    return self.recv() #recv message in response
+                return None
             except Exception:
                 raise
         else:
             raise NetworkException("Connection currently closed.")
 
     def recv(self):
-        if self._OPEN_STATUS:
+        """
+        Send message to other party over TCP
+
+        :return: response to message else None
+        """
+        if self._open_status:
             try:
-                amount_expected = int(self._socket.recv(16).decode()) #get length of incoming message
+                amount_expected = int(self._socket.recv(16).decode()) #get message length
                 amount_received = 0
                 message = ""
                 while amount_received < amount_expected:
@@ -44,20 +65,26 @@ class Connection:
             raise NetworkException("Connection currently closed.")
 
     def open(self):
-        if not self._OPEN_STATUS:
+        """
+        Open tcp connection with other party
+        """
+        if not self._open_status:
             try:
                 self._socket.connect((self._host, self._port))
-                self._OPEN_STATUS = True
+                self._open_status = True
             except Exception:
                 raise
         else:
             raise NetworkException("Connection is already open.")
 
     def close(self):
-        if self._OPEN_STATUS:
+        """
+        Close tcp connection
+        """
+        if self._open_status:
             try:
                 self._socket.close()
-                self._OPEN_STATUS = False
+                self._open_status = False
                 self._socket = socket(AF_INET, SOCK_STREAM)
             except Exception:
                 raise
@@ -65,7 +92,15 @@ class Connection:
             raise NetworkException("Connection currently closed.")
 
 class ConnectionHandler:
+    """
+    Class to handle incoming tcp connections
+    """
     def __init__(self, function):
+        """
+        Create base ConnectionHandler
+
+        :param function: callback function, called with arguments ((ip, host), Connection)
+        """
         self._function = function
         self._socket = socket(AF_INET, SOCK_STREAM)
         self._threads = []
@@ -75,9 +110,9 @@ class ConnectionHandler:
         self._stop_flag = False
         self._socket.bind((gethostbyname(gethostname()), port))
         self._socket.listen(10)
-        t = threading.Thread(name="handler", target=self.handler, args=())
-        t.start()
-        self._threads.append(t)
+        thread = threading.Thread(name="handler", target=self.handler, args=())
+        thread.start()
+        self._threads.append(thread)
 
     def handler(self):
         while not self._stop_flag:
@@ -90,9 +125,11 @@ class ConnectionHandler:
                 raise
             else:
                 client_connection = Connection(addr[0], addr[1], connection=conn)
-                t = threading.Thread(name="worker", target=self._function, args=(addr,client_connection))
-                t.start()
-                self._threads.append(t)
+                thread = threading.Thread(name="worker",
+                                          target=self._function,
+                                          args=(addr, client_connection))
+                thread.start()
+                self._threads.append(thread)
 
     def stop(self):
         self._stop_flag = True
