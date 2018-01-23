c78baaccff0c83fcc7a872cfdd87773ac3a8f91b
Barrett122 <2222658+barrett122@users.noreply.github.com>
2018-01-23 15:21:30 +0000

    Add Message class

--- client/src/ssl_client_connection.py
+++ client/src/ssl_client_connection.py
@@ -7,7 +7,8 @@ context.verify_mode = ssl.CERT_REQUIRED
 context.check_hostname = True
 context.load_verify_locations(os.path.join("..", "config", "ca-bundle.crt"))
 
-conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname="Team 7")
+conn = context.wrap_socket(socket.socket(socket.AF_INET),
+                           server_hostname="Team 7")
 conn.connect(("127.0.0.1", 10023))
 cert = conn.getpeercert()
 print(cert)
---
+++ shared/src/message.py
@@ -0,0 +1,58 @@
+"""A module for creating serialisable messages from simple objects."""
+
+from datetime import datetime
+import pickle
+
+
+class Message():
+    """A class representing a network message.
+
+    A class which models a single network-ready message formed from a
+    simple object.
+    """
+
+    def __init__(self, obj, player_id):
+        """Initialise a Message object.
+
+        :param obj: The object to be stored in the message.
+        """
+        self._obj = obj
+        self._id = player_id
+        self._type = str(obj.__class__)
+        self._timestamp = datetime.now()
+
+    def serialise(self):
+        """Serialise Message object to byte string.
+
+        :returns: bytes -- The serialised message value
+        """
+        return pickle.dumps(self)
+
+    @property("obj")
+    def get_obj(self):
+        """Return the stored object in this message."""
+        return self._obj
+
+    @property("id")
+    def get_id(self):
+        """Return the id in this message."""
+        return self._id
+
+    @property("type")
+    def get_type(self):
+        """Return the type of the object stored in this message."""
+        return self._type
+
+    @property("timestamp")
+    def get_timestamp(self):
+        """Return the timestamp at which this message was created."""
+        return self._timestamp
+
+    @staticmethod
+    def deserialise(bytestring):
+        """Deserialise byte string to Message object.
+
+        :param bytestring: bytes -- The string of bytes to be deserialised
+        to a message object.
+        """
+        return pickle.loads(bytestring)
