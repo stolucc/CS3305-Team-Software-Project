3fbedfff43191e1893a1587c15fdc1f5ff1a9ebd
chrisoc23 <christopher.oconnor23@gmail.com>
2018-01-24 11:51:46 +0000

    -Renamed MovementCost.py to Terrain.py
    Chris

--- shared/src/MovementCost.py
+++
@@ -1,9 +0,0 @@
-MOVEMENT_COSTS = {"flat": 1, "river": 2, "snow": 3, "hill": 5, "snowy hill": 8, "mountain": None}
-#mountain is impassable
-
-class Terrain:
-
-    def __init__(self, terrain, modifier):
-        self._terrain = terrain
-        self._modifier = modifier
-        self._cost = MOVEMENT_COSTS[modifier]
