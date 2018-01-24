ab666c0bfd8efc49c980726362eccaf3d5fb95b6
chrisoc23 <christopher.oconnor23@gmail.com>
2018-01-24 12:36:49 +0000

    - Refactored class "Terrain"
    - Removed global variables
    
    -Added comments
    Chris

--- shared/src/Terrain.py
+++ shared/src/Terrain.py
@@ -1,9 +1,45 @@
-MOVEMENT_COSTS = {"flat": 1, "river": 2, "snow": 3, "hill": 5, "snowy hill": 8, "mountain": None}
-#mountain is impassable
+"""Terrain"""
+
 
 class Terrain:
+    """
+    A class for terrain
+    """
+    def __init__(self, terrain, biome, movement_cost):
+        """
+        Create a new terrain object
 
-    def __init__(self, terrain, modifier):
+        :param terrain: the terrain type (eg. flat, hill, river, mountain, etc.)
+        :param biome: the terrain biome (eg. tundra, grassland, desert, etc.)
+        :param movement_cost: movement cost for units crossing this tile, calculated based on the terrain type
+        and biome type
+        """
+
         self._terrain = terrain
-        self._modifier = modifier
-        self._cost = MOVEMENT_COSTS[modifier]
+        self._biome = biome
+        self._movement_cost = movement_cost
+
+    @property
+    def terrain(self):
+        """Getter for terrain"""
+        return self._terrain
+
+    @property
+    def biome(self):
+        """Getter for biome"""
+        return self._biome
+
+    @property
+    def movement_cost(self):
+        """Getter for cost"""
+        return self._movement_cost
+
+    def __repr__(self):
+        return "Terrain:%s, Biome:%s, Movement Cost:%d\n" % (self.terrain, self.biome, self.movement_cost)
+
+
+if __name__ == '__main__':
+    terrain1 = Terrain("flat", "grassland", 1)
+    terrain2 = Terrain("hill", "desert", 3)
+    print(terrain1)
+    print(terrain2)
