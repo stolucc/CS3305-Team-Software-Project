49ec1a5a40af5b2ebec8a55baf6258b039cd3491
chrisoc23 <christopher.oconnor23@gmail.com>
2018-01-24 13:25:23 +0000

    - Added class global variables for movement cost values
    - Added function to calculate movement costs
    
    - Documentation
    Chris

--- shared/src/Terrain.py
+++ shared/src/Terrain.py
@@ -1,28 +1,31 @@
-"""Terrain"""
+"""Terrain class"""
 
 
 class Terrain:
+
+    TERRAIN_TYPE_MOVEMENT_COSTS = {"flat": 1, "hill": 2, "river": 2, "mountain": None}
+    BIOME_MOVEMENT_COSTS = {"tundra": 2, "grassland": 0, "desert": 1, "jungle": 1}
+
     """
     A class for terrain
     """
-    def __init__(self, terrain, biome, movement_cost):
+    def __init__(self, terrain_type, biome):
         """
         Create a new terrain object
 
-        :param terrain: the terrain type (eg. flat, hill, river, mountain, etc.)
+        :param terrain_type: the terrain type (eg. flat, hill, river, mountain, etc.)
         :param biome: the terrain biome (eg. tundra, grassland, desert, etc.)
-        :param movement_cost: movement cost for units crossing this tile, calculated based on the terrain type
         and biome type
         """
 
-        self._terrain = terrain
+        self._terrain_type = terrain_type
         self._biome = biome
-        self._movement_cost = movement_cost
+        self._movement_cost = self.calculate_movement_cost(terrain_type, biome)
 
     @property
-    def terrain(self):
+    def terrain_type(self):
         """Getter for terrain"""
-        return self._terrain
+        return self._terrain_type
 
     @property
     def biome(self):
@@ -34,12 +37,33 @@ class Terrain:
         """Getter for cost"""
         return self._movement_cost
 
+    def calculate_movement_cost(self, terrain_type, biome):
+        """
+        Calculates the combined movement cost of the terrain_type and biome
+
+        :param terrain_type: The terrain type (eg. flat, hill, river, mountain, etc.)
+        :param biome: the terrain biome (eg. tundra, grassland, desert, etc.)
+        :return: The combined movement cost of the terrain_type and biome, or None if terrain_type is mountain
+        """
+        terrain_movement_cost = self.TERRAIN_TYPE_MOVEMENT_COSTS[terrain_type]
+        biome_movement_cost = self.BIOME_MOVEMENT_COSTS[biome]
+        if terrain_movement_cost is None or biome_movement_cost is None:
+            return None
+        return terrain_movement_cost + biome_movement_cost
+
     def __repr__(self):
-        return "Terrain:%s, Biome:%s, Movement Cost:%d\n" % (self.terrain, self.biome, self.movement_cost)
+        string = "Terrain:%s, Biome:%s Movement Cost:" % (self.terrain_type, self.biome)
+        if self._movement_cost is None:
+            string += "Not traversable\n"
+        else:
+            string += "%d\n" % self.movement_cost
+        return string
 
 
 if __name__ == '__main__':
-    terrain1 = Terrain("flat", "grassland", 1)
-    terrain2 = Terrain("hill", "desert", 3)
+    terrain1 = Terrain("mountain", "tundra")
+    terrain2 = Terrain("hill", "desert",)
+    terrain3 = Terrain("flat", "grassland")
     print(terrain1)
     print(terrain2)
+    print(terrain3)
