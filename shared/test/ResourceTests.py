aca349fe31b2c6f3d87353023f7aaf8f220b319a
Alessia Nardotto <alessianard8@gmail.com>
2018-01-26 19:26:45 +0000

    PEP8 standard

--- shared/test/ResourceTests.py
+++ shared/test/ResourceTests.py
@@ -1,8 +1,6 @@
 """Resource Tests."""
 
 import unittest
-import sys
-sys.path.insert(0, "/Users/alessianardotto/projects/CS3305-Team-Software-Project/shared/src")
 from Resource import Resource
 
 
@@ -13,27 +11,24 @@ class ResourceTests(unittest.TestCase):
         """Test that available quantity matches given value"""
         res = Resource(5)
         self.assertEqual(res.quantity, 5)
-        
-        
+
     def testInitialWorkState(self):
         """Test that is_worked state is initially False"""
         res = Resource(5)
         self.assertEqual(res.is_worked, False)
-        
 
     def testWork(self):
         """Test that work method changes is_worked state to True"""
         res = Resource(5)
         res.work()
         self.assertEqual(res.is_worked, True)
-        
 
     def testStopWork(self):
         """Test that stop_work method changes is_worked state to False"""
         res = Resource(5)
         res.stop_work()
         self.assertEqual(res.is_worked, False)
-        
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
     unittest.main()
