2d4ff6b4b8529661dd15d690b6c976614ff2bb27
Alessia Nardotto <alessianard8@gmail.com>
2018-01-26 13:54:40 +0000

    Add comments for tests and fix minor bugs

--- shared/test/CurrencyTests.py
+++ shared/test/CurrencyTests.py
@@ -1,145 +1,113 @@
-import unittest
-import Currency
-
 """Currency Tests."""
 
+import unittest
+import sys
+sys.path.insert(0, "/Users/alessianardotto/projects/CS3305-Team-Software-Project/shared/src")
+from Currency import Currency
 
-class CurrencyTests:
 
+class CurrencyTests(unittest.TestCase):
     """Tests for the Currency class."""
 
-
-
     def testModifiersAreBothEmptyDictionariesCalledUnitAndImprovement(self):
-
+        """Test that modifiers are as expected."""
         cur = Currency(5, 1)
-
-        assertEquals(cur._modifiers, {"unit": {}, "improvement": {}})
-
+        self.assertEqual(cur._modifiers, {"unit": {}, "improvement": {}})
 
     def testBeginningValue(self):
-
+        """Test beginning value is the same as the one tested."""
         cur = Currency(5, 1)
-
-        assertEquals(cur.value(), 5)
-
+        self.assertEqual(cur.value, 5)
 
     def testBaseIncrease(self):
-
+        """Test base increase has the correct value. """
         cur = Currency(5, 1)
-
-        assertEquals(cur.base_increase(), 1)
-
+        self.assertEqual(cur.base_increase, 1)
 
     def testTurnIncrease(self):
-
+        """Test turn increase has the correct value."""
         cur = Currency(5, 1)
+        self.assertEqual(cur.turn_increase, 1)
 
-        assertEquals(cur.turn_increase(), 0)
-
-
-    def testAdd(self, increase):
-
+    def testAdd(self):
+        """Test that the add function adds to value."""
         cur = Currency(5, 1)
-
         cur.add(2)
+        self.assertEqual(cur.value, 7)
 
-        assertEquals(cur.value(), 7)
-
-
-    def testDeduct(self, decrease):
-
+    def testDeduct(self):
+        """Test that the deduct function deducts from value."""
         cur = Currency(5, 1)
-
         cur.deduct(2)
-
-        assertEquals(cur.value(), 3)
-
+        self.assertEqual(cur.value, 3)
 
     def testIncrement(self):
-
+        """Test that the increment adds turn increase to value."""
         cur = Currency(5, 1)
-
         cur.increment()
-
-        assertEquals(cur.value(), 5 + cur.turn_increase())
-
-
+        self.assertEqual(cur.value, 5 + cur.turn_increase)
 
     def testUpdateWithNoModifiers(self):
-
+        """Test Update with no modifiers leaves turn increase unchanged."""
         cur = Currency(5, 1)
-
         cur.update()
-
-        assertEquals(self.turn_increase(), 1)
-
-        
+        self.assertEqual(cur.turn_increase, 1)
 
     def testUpdateWithUnitModifiers(self):
-
+        """
+        Test Update with unit modifiers to check turn increase
+        has the correct value.
+        """
         cur = Currency(5, 1)
-
         cur.add_modifier("unit", "type1", 2)
-
         cur.update()
+        self.assertEqual(cur.turn_increase, 3)
 
-        assertEquals(self.turn_increase(), 3)
-
-
-        
     def testUpdateWithImprovementModifiers(self):
-
+        """
+        Test update with improvement modifiers to check turn
+        increase has the correct values.
+        """
         cur = Currency(5, 1)
-
         cur.add_modifier("improvement", "type2", 1)
-
         cur.update()
-
-        assertEquals(self.turn_increase(), 2)
-
-
+        self.assertEqual(cur.turn_increase, 2)
 
     def testUpdateWithBothModifiers(self):
-
+        """Test update with both types of modifier to check
+        test increase has the correct value."""
         cur = Currency(5, 1)
-
         cur.add_modifier("unit", "type1", 2)
-        
         cur.add_modifier("improvement", "type2", 1)
-
         cur.update()
-
-        assertEquals(self.turn_increase(), 4)
-
+        self.assertEqual(cur.turn_increase, 4)
 
     def testAddModifier(self):
-
+        """Test add modifier to ensure modifier is added to list."""
         cur = Currency(5, 1)
-
         cur.add_modifier("unit", "type1", 2)
-
-        assertEquals(self.modifiers(), {"unit": {"type1": 2}, "improvement": {}})
-
-
+        self.assertEqual(cur.modifiers,
+                         {"unit": {"type1": 2}, "improvement": {}})
 
     def testRemoveModifier(self):
-
+        """
+        Test remove modifier to ensure the modifier is removed
+        from the list.
+        """
         cur = Currency(5, 1)
-
         cur.add_modifier("unit", "type1", 2)
-
         cur.remove_modifier("unit", "type1")
-
-        assertEquals(self.modifiers(), {"unit": {}, "improvement": {}})
-
-
+        self.assertEqual(cur.modifiers, {"unit": {}, "improvement": {}})
 
     def testRemoveModifierOnNonExistentItem(self):
-
+        """
+        Test remove modifier on nonexistent modifier to ensure
+        the right exception is thrown.
+        """
         cur = Currency(5, 1)
-
-        cur.remove_modifier("improvement", "type1")
-
-        assertThrows(KeyError)
+        cur.remove_modifier("improvement", "type2")
+        self.assertThrows(KeyError)
         
+
+if __name__ == '__main__':
+    unittest.main()
