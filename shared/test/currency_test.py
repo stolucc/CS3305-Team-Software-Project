ec5f58b632021334a2e959ae12535d1c7454e787
ChoncyBear <choncy@rocketmail.com>
2018-01-29 12:55:15 +0000

    Add enum for modifier types and all associated code, pep8 fixs

--- shared/test/currency_test.py
+++ shared/test/currency_test.py
@@ -1,16 +1,17 @@
 """Currency Tests."""
 
 import unittest
-from currency import Currency
+from currency import Currency, ModifierType
 
 
 class CurrencyTests(unittest.TestCase):
     """Tests for the Currency class."""
 
-    def test_modifiers_are_both_empty_dictionaries_called_unit_and_improvement(self):
+    def test_empty_modifiers(self):
         """Test that modifiers are as expected."""
         cur = Currency(5, 1)
-        self.assertEqual(cur._modifiers, {"unit": {}, "improvement": {}})
+        self.assertEqual(cur._modifiers, {ModifierType.UNIT: {},
+                                          ModifierType.IMPROVEMENT: {}})
 
     def test_beginning_value(self):
         """Test beginning value is the same as the one tested."""
@@ -18,7 +19,7 @@ class CurrencyTests(unittest.TestCase):
         self.assertEqual(cur.value, 5)
 
     def test_base_increase(self):
-        """Test base increase has the correct value. """
+        """Test base increase has the correct value."""
         cur = Currency(5, 1)
         self.assertEqual(cur.base_increase, 1)
 
@@ -52,60 +53,49 @@ class CurrencyTests(unittest.TestCase):
         self.assertEqual(cur.turn_increase, 1)
 
     def test_update_with_unit_modifiers(self):
-        """
-        Test Update with unit modifiers to check turn increase
-        has the correct value.
-        """
+        """Test Update with unit modifiers."""
         cur = Currency(5, 1)
-        cur.add_modifier("unit", "type1", 2)
+        cur.add_modifier(ModifierType.UNIT, "type1", 2)
         cur.update()
         self.assertEqual(cur.turn_increase, 3)
 
     def test_update_with_improvement_modifiers(self):
-        """
-        Test update with improvement modifiers to check turn
-        increase has the correct values.
-        """
+        """Test update with improvement modifiers."""
         cur = Currency(5, 1)
-        cur.add_modifier("improvement", "type2", 1)
+        cur.add_modifier(ModifierType.IMPROVEMENT, "type2", 1)
         cur.update()
         self.assertEqual(cur.turn_increase, 2)
 
     def test_update_with_both_modifiers(self):
-        """Test update with both types of modifier to check
-        test increase has the correct value."""
+        """Test update with both types of modifier."""
         cur = Currency(5, 1)
-        cur.add_modifier("unit", "type1", 2)
-        cur.add_modifier("improvement", "type2", 1)
+        cur.add_modifier(ModifierType.UNIT, "type1", 2)
+        cur.add_modifier(ModifierType.IMPROVEMENT, "type2", 1)
         cur.update()
         self.assertEqual(cur.turn_increase, 4)
 
     def test_add_modifier(self):
         """Test add modifier to ensure modifier is added to list."""
         cur = Currency(5, 1)
-        cur.add_modifier("unit", "type1", 2)
+        cur.add_modifier(ModifierType.UNIT, "type1", 2)
         self.assertEqual(cur.modifiers,
-                         {"unit": {"type1": 2}, "improvement": {}})
+                         {ModifierType.UNIT: {"type1": 2},
+                          ModifierType.IMPROVEMENT: {}})
 
     def test_remove_modifier(self):
-        """
-        Test remove modifier to ensure the modifier is removed
-        from the list.
-        """
+        """Test remove modifier."""
         cur = Currency(5, 1)
-        cur.add_modifier("unit", "type1", 2)
-        cur.remove_modifier("unit", "type1")
-        self.assertEqual(cur.modifiers, {"unit": {}, "improvement": {}})
+        cur.add_modifier(ModifierType.UNIT, "type1", 2)
+        cur.remove_modifier(ModifierType.UNIT, "type1")
+        self.assertEqual(cur.modifiers, {ModifierType.UNIT: {},
+                                         ModifierType.IMPROVEMENT: {}})
 
     def test_remove_modifier_on_non_existent_item(self):
-        """
-        Test remove modifier on nonexistent modifier to ensure
-        the right exception is thrown.
-        """
+        """Test remove modifier on nonexistent modifier."""
         cur = Currency(5, 1)
         with self.assertRaises(KeyError):
-            cur.remove_modifier("improvement", "type2")
-        
+            cur.remove_modifier(ModifierType.IMPROVEMENT, "type2")
+
 
 if __name__ == '__main__':
     unittest.main()
