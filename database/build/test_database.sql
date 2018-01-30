ada75bd5082104d7277fd8562f387d8fcf7eb82a
Daragh O'Sullivan <daraghsullivan@gmail.com>
2018-01-30 11:27:54 +0000

    Add test database files and code

---
+++ database/build/test_database.sql
@@ -0,0 +1,11 @@
+-- Database: test
+
+-- DROP DATABASE test;
+
+CREATE DATABASE test
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_IE.UTF-8'
+       LC_CTYPE = 'en_IE.UTF-8'
+       CONNECTION LIMIT = -1;
