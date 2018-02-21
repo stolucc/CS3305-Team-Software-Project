ea25189d73c6cb35fed182a6a16c79f25b0f77f6
Daragh O'Sullivan <daraghsullivan@gmail.com>
2018-02-21 15:28:38 +0000

    Create initial GameState object

---
+++ shared/src/gamestate.py
@@ -0,0 +1,73 @@
+"""Game state representation."""
+
+
+class GameState:
+    """Game state class."""
+
+    def __init__(self, game_id, seed, grid):
+        """
+        Initialise GameState attributes.
+
+        :param game_id: hex grid that game is using
+        :param seed: hex grid that game is using
+        :param grid: hex grid that game is using
+        """
+        self._game_id = game_id
+        self._seed = seed
+        self._grid = grid
+        self._civs = {}
+        self._my_id = None
+
+    @property
+    def game_id(self):
+        """
+        Getter for game_id.
+
+        :return: game_id
+        """
+        return self._game_id
+
+    @property
+    def seed(self):
+        """
+        Getter for seed.
+
+        :return: seed
+        """
+        return self._seed
+
+    @property
+    def grid(self):
+        """
+        Getter for grid.
+
+        :return: grid
+        """
+        return self._grid
+
+    def get_civ(self, civ_id):
+        """
+        Return the civ with the id of civ_id.
+
+        :return: Civilization
+        """
+        return self._civs[civ_id]
+
+    # TODO Remove redundant civ_id when id has been added to Civilization.
+    def add_civ(self, civ_id, civ):
+        """Add the civ to the list of civs playing."""
+        self._civs[civ_id] = civ
+
+    @property
+    def my_id(self):
+        """
+        Getter for my_id.
+
+        :return: my_id
+        """
+        return self._my_id
+
+    @my_id.setter
+    def my_id(self, my_id):
+        """Setter for my_id."""
+        self._my_id = my_id
