347b310c78ad297f514b9c65e4f963ea85a5cf5f
Daragh O'Sullivan <daraghsullivan@gmail.com>
2018-03-02 14:35:20 +0000

    Restructure client code

--- client/src/main.py
+++ client/src/main.py
@@ -4,28 +4,12 @@ import action
 import sys
 
 
-class Client:
-    """Client object."""
-
-    def __init__(self):
-        """Create a new Class object."""
-        self._server_API = ServerAPI()
-
-    def join_game(self):
-        """Ask the server to join a game."""
-        self._server_API.join_game()
-
-    def leave_game(self):
-        """Ask the server to leave a game."""
-        self._server_API.leave_game()
-
-
 def main():
     """Start main program."""
-    client = Client()
+    server_api = ServerAPI()
     try:
-        client.join_game()
-        # client.leave_game()
+        server_api.join_game()
+        # server_api.leave_game()
     except action.ServerError as e:
         print("Server error occurred with error code " + str(e.error_code))
         sys.exit(1)
