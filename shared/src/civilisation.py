a9a679314fa12213527da194cdf26d923a578ee0
ShayVD <shay.vandam96@gmail.com>
2018-03-08 18:03:09 +0000

    Fix city destruction

--- shared/src/civilisation.py
+++ shared/src/civilisation.py
@@ -1,6 +1,6 @@
 """Civilisation representation."""
 
-from unit import Worker, Swordsman, Unit
+from unit import Worker, Swordsman, Soldier, Unit
 from city import City
 from building import Building
 from currency import CurrencyType
@@ -289,8 +289,9 @@ class Civilisation(object):
                 tile.unit = unit
                 pos.unit = None
                 unit.actions -= 1
-                if tile.city_id is not None and tile.civ_id != self._id:
-                    self.destroy_city(tile)
+                if tile.city_id is not None and isinstance(unit, Soldier)\
+                        and tile.civ_id != self._id:
+                    return self.destroy_city(tile)
             else:
                 self._logger.debug("Unable to move unit.")
         else:
@@ -308,6 +309,7 @@ class Civilisation(object):
         for city_tile in city_tiles:
             self.destroy_building(city_tile)
         self.destroy_building(tile)
+        return city_tiles
 
     def movement_cost_of_path(self, path):
         """Calculate movement cost of list of hex tiles."""
