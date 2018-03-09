f7dac9b272a1897959189727df350941a090fa76
Aaron O'Donoghue <aaronodonoghue2k11@gmail.com>
2018-03-09 13:48:40 +0000

    Remove music from menu.py

--- client/src/menu.py
+++ client/src/menu.py
@@ -157,12 +157,6 @@ if __name__ == "__main__":
                          ("Help", test), ("Exit", sys.exit)])
     menu.display_menu()
 
-    import music
-
-    song = "../resources/music/Egmont_Overture_Op_84.mp3"
-    pygame.mixer.init()
-    music.Music(song)
-
     while True:
         for event in pygame.event.get():   # User did something
             if event.type == pygame.QUIT:
