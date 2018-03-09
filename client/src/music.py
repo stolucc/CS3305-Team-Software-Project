712f3dac04cd0659eab034a94ea0da50f0d96ad8
Aaron O'Donoghue <aaronodonoghue2k11@gmail.com>
2018-03-09 13:41:38 +0000

    Fixed linter errors

--- client/src/music.py
+++ client/src/music.py
@@ -1,42 +1,41 @@
+"""Module for playing music and adding songs to Pygame display screens."""
 import pygame
 import sys
-from pygame.locals import *
 
 # Note: Songs must have a pygame display in order to work.
 # For the main menu, we could use the menu's display
 
 
 class Music:
-
-    """Plays a song"""
+    """Play a song."""
 
     def __init__(self, song):
-        """Creates a song and starts playing it"""
+        """Create a song and start playing it."""
         pygame.mixer.music.load(song)
         self.play()
 
     def play(self):
-        """Plays the song indefinitely"""
+        """Play the song indefinitely."""
         pygame.mixer.music.play(-1)
 
     def pause(self):
-        """Pauses the song temporarily"""
+        """Pause the song temporarily."""
         pygame.mixer.music.pause()
 
     def stop(self):
-        """Stops the song and goes back to the start"""
+        """Stop the song and go back to the start."""
         pygame.mixer.music.stop()
 
     def mute(self):
-        """Mutes the song"""
+        """Mute the song."""
         pygame.mixer.music.set_volume(0)
 
     def unpause(self):
-        """Unpauses the song"""
+        """Unpause the song."""
         pygame.mixer.music.unpause()
 
     def unmute(self):
-        """Unmutes the song"""
+        """Unmute the song."""
         pygame.mixer.music.set_volume(1)
 
 
@@ -48,7 +47,7 @@ if __name__ == "__main__":
     while True:   # Main Loop
 
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
         pygame.display.update()
