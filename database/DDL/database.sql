19465d332713a9a13cdff6b7725287b197d09fff
Daragh O'Sullivan <daraghsullivan@gmail.com>
2018-03-09 16:53:22 +0000

    Update database scripts

--- database/DDL/database.sql
+++ database/DDL/database.sql
@@ -6,6 +6,4 @@ CREATE DATABASE gamedb
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'en_IE.UTF-8'
-       LC_CTYPE = 'en_IE.UTF-8'
        CONNECTION LIMIT = -1;
